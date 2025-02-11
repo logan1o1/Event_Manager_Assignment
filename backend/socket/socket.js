04805b61b6e5881859641a3a9f68a7046e3d1f6b
Saswat Mahapatra <92862073+logan1o1@users.noreply.github.com>
2025-02-11 17:45:31 +0530

    minor fixes

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -11,18 +11,10 @@ const setupSocket = (server) => {
   io.on('connection', (socket) => {
     console.log(`Client connected: ${socket.id}`);
 
-    // client joins an event
     socket.on('joinEvent', (eventId) => {
       socket.join(eventId);
       console.log(`Socket ${socket.id} joined event room: ${eventId}`);
     });
-
-    // Listen for attendee count updates after attendee is added
-    // socket.on('updateAttendeeCount', ({ eventId, count }) => {
-    //   // Broadcast the updated count
-    //   io.to(eventId).emit('attendeeCountUpdate', { eventId, count });
-    //   console.log(`Updated attendee count for event ${eventId}: ${count}`);
-    // });
 
     socket.on('disconnect', () => {
       console.log(`Client disconnected: ${socket.id}`);
